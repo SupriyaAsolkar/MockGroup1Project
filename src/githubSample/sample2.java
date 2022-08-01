9feff6e55ca4bb8c093fb191bfc63e5af73e04a8
lenovo <lenovo@Manisha>
2022-08-01 14:35:41 +0530

    <Manisha> new changes created in sample2

--- src/githubSample/sample2.java
+++ src/githubSample/sample2.java
@@ -2,6 +2,6 @@ package githubSample;
 
 public class sample2 {
 public static void main(String[] args) {
-	System.out.println("work hard ");
+	System.out.println("work hard and get success ");
 }
 }
